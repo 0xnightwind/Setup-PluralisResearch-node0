4fbc7ba4729e84a0e2f265994d46b40df5b57f33
0xnightwind <nightwind1899@gmail.com>
2025-09-28 17:24:09 +0000

    add basics
    Signed-off-by: 0xnightwind <nightwind1899@gmail.com>

---
+++ basics.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev  -y
+
+sudo apt install -y python3-pip
+sudo apt install pip
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+
+source ~/miniconda3/bin/activate
+
+conda init --all
+
